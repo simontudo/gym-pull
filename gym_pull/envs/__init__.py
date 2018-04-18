eaeb16631dc12128ea860c396b88bd57ef78d66c
Simon <simon.hakenes@tu-dortmund.de>
2018-04-18 13:47:48 +0200

    clean up

--- gym_pull/envs/__init__.py
+++ gym_pull/envs/__init__.py
@@ -1,4 +1,2 @@
 from gym_pull.envs.registration import registry, register, make, spec
-# >>>>>>>>> START changes >>>>>>>>>>>>>>>>>>>>>>>>
 from gym_pull.envs.registration import deregister, list
-# <<<<<<<<< END changes <<<<<<<<<<<<<<<<<<<<<<<<<<
